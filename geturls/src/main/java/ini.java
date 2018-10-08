79d3ba8b31b3d98d72c3d7d2cb71a0d9fa8aacc9
laiwch <344824257@qq.com>
2018-10-08 18:29:38 +0200

    read tif && modify ini.java && change lib

--- geturls/src/main/java/ini.java
+++ geturls/src/main/java/ini.java
@@ -6,14 +6,35 @@ import org.gdal.gdal.gdal;
 import org.gdal.gdalconst.gdalconst;
 import org.gdal.gdalconst.gdalconstConstants;
 
+import java.io.Console;    
+
 public class ini{
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		ini test = new ini();
+		
 		gdal.AllRegister();
-		return 0;
+
+		//String filename =  "/home/laiwch/lsde/data/187/014/LC08_L1TP_187014_20170710_20170710_01_RT/LC08_L1TP_187014_20170710_20170710_01_RT_B1.TIF";
+		String filename = "LC08_L1TP_187014_20170710_20170710_01_RT_B1.TIF";
+		Dataset hDataset = gdal.Open(filename, gdalconstConstants.GA_ReadOnly);
+		if (hDataset == null)
+		{
+			System.err.println("GDALOpen failed - " + gdal.GetLastErrorNo());
+			System.err.println(gdal.GetLastErrorMsg());
+
+			System.exit(1);
+		}
+		Driver hDriver = hDataset.GetDriver();
+		System.out.println("hello world!");
+
+		System.out.println("Driver: " + hDriver.getShortName() + "/" + hDriver.getLongName());
+
+		System.out.println("Size is " + hDataset.getRasterXSize() + ", "
+				+ hDataset.getRasterYSize());
+
+		hDataset.delete();
+
+		gdal.GDALDestroyDriverManager();
 	}
 
 }
